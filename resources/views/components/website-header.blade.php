d86f72137d8e440224cb77c961986d04c1f38a31
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2021-12-28 17:58:57 -0800

    Actualizacion

--- resources/views/components/website-header.blade.php
+++ resources/views/components/website-header.blade.php
@@ -1,7 +1,7 @@
 <header class="pt-12 pb-0 md:py-16">
     <div class="w-10/12 mx-auto flex flex-col space-y-5 md:space-y-0 md:flex-row items-start md:items-center">
         <div>
-            <a href="{{ route('home') }}" class="text-xl md:text-3xl font-bold">Jonathan Velazquez</a>
+            <a href="{{ route('home') }}" class="text-2xl md:text-3xl font-bold">Jonathan Velazquez</a>
         </div>
 
         <nav class="ml-0 md:ml-auto text-base md:text-xl flex items-center space-x-5 md:space-x-12">
--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -2,7 +2,7 @@
 	<section class="py-7 md:py-20">
 		<div class="w-10/12 mx-auto">
 			<div class="w-full md:w-9/12">
-				<div class="text-4xl md:text-8xl font-bold leading-tight">
+				<div class="text-2xl leading-relaxed md:text-8xl font-bold md:leading-tight">
 					@if($coverPage)
 					{!! $coverPage->body !!}
 					@endif
